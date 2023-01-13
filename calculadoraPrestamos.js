5907677fa13ad677d1005483e77a8dcd3d2cb17e
Alexis <alexisperez132397@gmail.com>
2023-01-12 21:17:47 -0300

    pasando commit 2

--- calculadoraPrestamos.js
+++ calculadoraPrestamos.js
@@ -1,2 +1,25 @@
-let nombre_usuario = "ALEX";
-let constraseña_usuario = "1234";
+let mensaje = parseInt(prompt("Bienvenidos a $ExtraCash\n 1. Ingresar a su cuenta\n 2. Registrarse"));
+
+const userAdmin = "admin";
+const passAdmin = "admin";
+
+
+switch (mensaje) {
+    case 1:
+        let monto = parseFloat(prompt("Ingrese el Monto a retitrar"));
+        let cuotas = parseInt(prompt("Ingrese la cantidad de cuotas\n 3 Coutas\n 6 Cuotas\n 12 Cuotas"));
+        let interes = parseFloat(prompt("Ingrese el Porcentaje de intereses"));
+        let total = (monto * interes)/cuotas;
+        for (let i = 1; i <= cuotas; i++){
+            console.log(`cuota ${i}, monto a abonar ${total}`);
+        }
+        console.log(`monto total a Abonar es de ${monto}`);
+        break;
+
+    case 2:
+        userNew = parseInt(prompt("Ingrese un Nombre de usuario en minusculas"));
+        passNew = parseInt(prompt("Ingrese una Contraseña de 4 digitos"));
+        break;
+    default:
+        break;
+}
